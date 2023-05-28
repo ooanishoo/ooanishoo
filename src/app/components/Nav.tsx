3de040d0c49e24f1f711e4ae6ef9422e5d348f80
Anish <anish.maharjan@zendesk.com>
2023-05-28 14:05:44 +1000

    feat(nav): dynamically render navigations

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,13 +1,41 @@
 import Image from 'next/image'
+import Link from 'next/link'
+
+interface INavigation {
+  title: string
+  link: string
+}
+const NAVIGATIONS: INavigation[] = [
+  {
+    title: 'About',
+    link: '/about',
+  },
+  {
+    title: 'Testimonials',
+    link: '/testimonials',
+  },
+  {
+    title: 'Tools',
+    link: '/tools',
+  },
+  {
+    title: 'Contact',
+    link: '/contact',
+  },
+]
 
 export const Nav = () => (
   <nav className="flex flex-row justify-between items-center pt-6">
-    <Image src="/ooanishoo.png" alt="me" width="64" height="64" />
-    <ul className="flex justify-end flex-auto  gap-12 px-6 items-center">
-      <li>About</li>
-      <li>Testimonials</li>
-      <li>Tools</li>
-      <li>Contact</li>
+    <Link href={'/'}>
+      <Image src="/ooanishoo.png" alt="me" width="64" height="64" />
+    </Link>
+
+    <ul className="flex justify-end flex-auto  gap-12 px-6 items-center c">
+      {NAVIGATIONS.map((nav, _index) => (
+        <li key={_index}>
+          <Link href={nav.link}>{nav.title}</Link>
+        </li>
+      ))}
     </ul>
     <div className="px-4">Dark mode</div>
   </nav>
