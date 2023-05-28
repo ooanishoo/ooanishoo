0dbda53664e271331c88d8fb08a63fa7216d315c
Anish <anish.maharjan@zendesk.com>
2023-05-28 15:57:08 +1000

    feat: add support to show different logos based on current theme

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import ToogleThemeButton from './ToggleThemeButton'
+import { useTheme } from 'next-themes'
 
 interface INavigation {
   title: string
@@ -26,19 +27,27 @@ const NAVIGATIONS: INavigation[] = [
   },
 ]
 
-export const Nav = () => (
-  <nav className="flex flex-row justify-between items-center pt-6 gap-6">
-    <Link href={'/'}>
-      <Image src="/ooanishoo.png" alt="me" width="64" height="64" />
-    </Link>
+export const Nav = () => {
+  const { theme } = useTheme()
+  return (
+    <nav className="flex flex-row justify-between items-center pt-6 gap-6">
+      <Link href={'/'}>
+        <Image
+          src={theme === 'light' ? '/logo-black.png' : '/logo-white.png'}
+          alt="me"
+          width="64"
+          height="64"
+        />
+      </Link>
 
-    <ul className="flex justify-end flex-auto  gap-12 px-6 items-center c">
-      {NAVIGATIONS.map((nav, _index) => (
-        <li key={_index}>
-          <Link href={nav.link}>{nav.title}</Link>
-        </li>
-      ))}
-    </ul>
-    <ToogleThemeButton />
-  </nav>
-)
+      <ul className="flex justify-end flex-auto  gap-12 px-6 items-center c">
+        {NAVIGATIONS.map((nav, _index) => (
+          <li key={_index}>
+            <Link href={nav.link}>{nav.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <ToogleThemeButton />
+    </nav>
+  )
+}
