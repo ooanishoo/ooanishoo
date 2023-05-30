3e01d383085dd0280d2fb15c206e60a9b06b332a
Anish <anish.maharjan@zendesk.com>
2023-05-30 11:02:27 +1000

    feat(Markdown): add feature to read markdown file and render into html
    - install markdown-to-jsx
    - install @tailwindcss/typograph

--- src/app/toolbox/page.tsx
+++ src/app/toolbox/page.tsx
@@ -1,3 +1,12 @@
+import Markdown from 'markdown-to-jsx'
+import { getMarkdownContent } from '../helpers'
+
 export default function ToolBox() {
-  return <div>ToolBox</div>
+  const content = getMarkdownContent('toolbox/tech.md')
+
+  return (
+    <article className="prose md:prose-lg lg:prose-xl prose-headings:text-4xl prose-headings:font-normal dark:prose-invert ">
+      <Markdown>{content}</Markdown>
+    </article>
+  )
 }
