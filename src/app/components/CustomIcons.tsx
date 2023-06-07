2df7e9c3fdb9ac95851597eb7009fe74bcf53f42
Anish <anish.maharjan@zendesk.com>
2023-06-08 09:10:36 +1000

    chore: add menu icon

--- src/app/components/CustomIcons.tsx
+++ src/app/components/CustomIcons.tsx
@@ -51,3 +51,23 @@ export const SunIcon = (props: React.SVGProps<SVGSVGElement>) => (
     </g>
   </svg>
 )
+
+// icon:arrows_hamburger 2 | Linea Iconset https://linea.io/ | Benjamin Sigidi
+
+export const MenuIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 64 64"
+    fill="currentColor"
+    height="1em"
+    width="1em"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d="M12 21h40M12 33h40M12 45h40"
+    />
+  </svg>
+)
