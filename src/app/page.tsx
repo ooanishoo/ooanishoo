70f872c820b628377d285d5ffb0abf644e8e8832
Anish <anish.maharjan@zendesk.com>
2023-06-08 17:06:28 +1000

    style(Home): update padding and fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="mt-2 flex flex-col items-center gap-12 sm:items-start md:flex-row ">
+    <div className="mt-2 flex flex-col items-center gap-4 sm:items-start sm:gap-6 md:flex-row lg:gap-8 ">
       <Image
         src={profilePic}
         alt="Picture of Anish Maharjan"
         className="w-full max-w-2xl rounded-3xl shadow-lg duration-700 md:max-w-sm lg:max-w-lg"
       />
       <div className="flex-auto">
-        <h2 className="pt-2 text-4xl font-extrabold text-black dark:text-white">{`Hi, I'm Anish. Nice to meet you!`}</h2>
+        <h2 className="pt-2 text-2xl font-extrabold text-black dark:text-white sm:text-3xl lg:text-4xl">{`Hi, I'm Anish. Nice to meet you!`}</h2>
         <p className="mt-6 text-lg text-gray-800 dark:text-slate-400 sm:mt-10 xl:max-w-3xl">
           {`I'm a Software Engineer based in Melbourne. I love to design and write clean codes. I'm passionate about building useable products that solve real customer problems.`}
         </p>
