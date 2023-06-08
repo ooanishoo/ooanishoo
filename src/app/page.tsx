e9c4f37b9f9300e7bc5a7c64a8055533d47df79d
Anish <anish.maharjan@zendesk.com>
2023-06-08 17:08:32 +1000

    style(Layout): update margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
       />
       <div className="flex-auto">
         <h2 className="pt-2 text-2xl font-extrabold text-black dark:text-white sm:text-3xl lg:text-4xl">{`Hi, I'm Anish. Nice to meet you!`}</h2>
-        <p className="mt-6 text-lg text-gray-800 dark:text-slate-400 sm:mt-10 xl:max-w-3xl">
+        <p className="mt-6 text-sm text-gray-800 dark:text-slate-400 sm:mt-10 sm:text-lg xl:max-w-3xl">
           {`I'm a Software Engineer based in Melbourne. I love to design and write clean codes. I'm passionate about building useable products that solve real customer problems.`}
         </p>
-        <p className="mt-6 py-2 text-lg font-semibold text-slate-700 hover:text-sky-500 dark:text-slate-200  dark:hover:text-sky-400 lg:mt-10">
+        <p className="mt-6 py-2 text-sm font-semibold text-slate-700 hover:text-sky-500 dark:text-slate-200 dark:hover:text-sky-400  sm:text-lg lg:mt-10">
           <Link href="/about">Learn more about me</Link>
         </p>
       </div>
