998f5119033d0549a093d51f44577dba77b7bed1
Anish <anish.maharjan@zendesk.com>
2023-06-08 16:58:46 +1000

    style: make uses text look smaller on smaller device

--- src/app/uses/page.tsx
+++ src/app/uses/page.tsx
@@ -5,7 +5,7 @@ export default function ToolBox() {
   const content = getMarkdownContent('docs/uses.md')
 
   return (
-    <article className="prose prose-base mx-auto dark:prose-invert lg:container prose-h1:text-2xl prose-h2:text-xl prose-a:no-underline hover:prose-a:text-sky-500 dark:hover:prose-a:text-sky-400">
+    <article className="prose prose-sm mx-auto dark:prose-invert sm:container md:prose-base prose-h1:text-2xl prose-h2:text-xl prose-a:no-underline hover:prose-a:text-sky-500 dark:hover:prose-a:text-sky-400">
       <Markdown>{content}</Markdown>
     </article>
   )
