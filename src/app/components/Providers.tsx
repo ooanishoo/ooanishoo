5a1024e193b8cfe03f0a262e35aefc8a19ae4a97
Anish <anish.maharjan@zendesk.com>
2023-06-21 20:09:21 +1000

    style(theme): use dark mode by default

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -3,7 +3,11 @@ import { ThemeProvider } from 'next-themes'
 import { ReactNode } from 'react'
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>
+  return (
+    <ThemeProvider defaultTheme="dark" attribute="class">
+      {children}
+    </ThemeProvider>
+  )
 }
 
 export default Providers
