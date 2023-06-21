79cf5e3b3ff1a5991a4259d46308c4e8338fb5de
Anish <anish.maharjan@zendesk.com>
2023-06-21 20:10:45 +1000

    docs: wip contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,15 @@
 export default function Contact() {
-  return <div>Contact</div>
+  return (
+    <div>
+      <h2 className="pt-2 text-2xl font-extrabold text-black dark:text-white sm:text-3xl lg:text-4xl">
+        {`Let's connect!`}
+      </h2>
+      <p className=" mb-8 mt-6 text-sm text-gray-800 dark:text-slate-400  sm:text-lg">
+        {`If you're a kindred spirit who appreciates the beauty of both technology and music, I'd love to connect. We can chat about anything from our favorite coding languages, your best workout routines or even share a few guitar chords.`}
+      </p>
+      <p className=" mb-8 mt-6 text-sm text-gray-800 dark:text-slate-400  sm:text-lg">
+        {`Oh, and I must admit, I have a soft spot for nerdy jokes and references!`}
+      </p>
+    </div>
+  )
 }
