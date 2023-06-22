1fe15ba8ea0a76d124d892c283e04c9cc61c207c
Anish <anish.maharjan@zendesk.com>
2023-06-22 22:37:37 +1000

    feat(Footer): display ToggleThemeButton on the footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,37 +1,9 @@
+'use client'
+
 import { IconType } from 'react-icons'
 import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
 import { SiPolywork } from 'react-icons/si'
-
-export const Footer = () => {
-  return (
-    <footer className="mt-16 px-4 text-sm sm:px-6 lg:px-8">
-      <FooterContent />
-    </footer>
-  )
-}
-
-const FooterContent = () => (
-  <div className="justify-between border-t border-solid border-slate-200 pb-28 pt-8 text-slate-500 dark:border-slate-200/5 sm:flex">
-    <div className="mb-6 sm:mb-0 sm:flex">
-      <p>&copy; Anish Maharjan {new Date().getFullYear()}</p>
-    </div>
-    <div className="flex space-x-6 text-slate-400 dark:text-slate-500">
-      {socialLinks.map((socialLink, index) => (
-        <a
-          href={socialLink.url}
-          target="_blank"
-          key={index}
-          title={socialLink.name}
-        >
-          <socialLink.icon
-            size={24}
-            className=" hover:text-slate-500 dark:hover:text-slate-400"
-          />
-        </a>
-      ))}
-    </div>
-  </div>
-)
+import ToggleThemeButton from './ToggleThemeButton'
 
 interface SocialLink {
   name: string
@@ -61,3 +33,34 @@ const socialLinks: SocialLink[] = [
     icon: FaTwitter,
   },
 ]
+
+export const Footer = () => {
+  return (
+    <footer className="mt-16 px-4 text-sm sm:px-6 lg:px-8">
+      <FooterContent />
+    </footer>
+  )
+}
+
+const FooterContent = () => (
+  <div className="flex flex-col items-center justify-between border-t border-solid border-slate-200 pb-28 pt-8 text-slate-500 dark:border-slate-200/5 sm:flex-row">
+    <div className="mb-6 flex items-center gap-2 whitespace-nowrap sm:mb-0">
+      <p>&copy; Anish Maharjan {new Date().getFullYear()}</p>
+      <ToggleThemeButton size={16} />
+    </div>
+    <div className="flex space-x-6 text-slate-400 dark:text-slate-500">
+      {renderSocialLinks()}
+    </div>
+  </div>
+)
+
+const renderSocialLinks = () =>
+  socialLinks.map((socialLink, index) => (
+    <a href={socialLink.url} target="_blank" key={index}>
+      <socialLink.icon
+        title={socialLink.name}
+        size={24}
+        className=" hover:text-slate-500 dark:hover:text-slate-400"
+      />
+    </a>
+  ))
